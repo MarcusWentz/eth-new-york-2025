592537834833e7d0dcb45e8e9ab5cd114963dacf
dinadeljanin <32048693+dinadeljanin@users.noreply.github.com>
2025-08-16 20:13:30 -0400

    not now tiger, mummy is moving the ui dir over from one repo into the other

---
+++ ui/src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<header>
+				<div></div>
+			</header>
+			<main>
+				<div className="ellipsis" />
+				<section />
+			</main>
+			<footer>
+				<div />
+			</footer>
+		</>
+	);
+}
+
+export default App;
